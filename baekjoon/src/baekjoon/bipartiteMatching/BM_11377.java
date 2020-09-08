81447dcf00f70102d6bd6eb47bfb46e208b8f451
unknown <yuhyemin96@gmail.com>
2020-09-08 16:32:10 +0900

    200908 이분매칭 기초 문제 풀이

---
+++ baekjoon/src/baekjoon/bipartiteMatching/BM_11377.java
@@ -0,0 +1,95 @@
+package baekjoon.bipartiteMatching;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * 열혈강호 3
+ * 강호네 회사에는 직원이 N명이 있고, 해야할 일이 M개가 있다. 직원은 1번부터 N번까지 번호가 매겨져 있고, 일은 1번부터 M번까지 번호가 매겨져 있다.
+ * 각 직원은 한 개의 일만 할 수 있고, 각각의 일을 담당하는 사람은 1명이어야 한다. 
+ * 단, N명 중에서 K명은 일을 최대 2개할 수 있다.
+ * 각각의 직원이 할 수 있는 일의 목록이 주어졌을 때, M개의 일 중에서 최대 몇 개를 할 수 있는지 구하는 프로그램을 작성하시오.
+ * 
+ * [입력]
+ * 첫째 줄에 직원의 수 N과 일의 개수 M, 일을 2개할 수 있는 직원의 수 K가 주어진다. (1 ≤ N, M ≤ 1,000, 1 ≤ K ≤ N)
+ * 둘째 줄부터 N개의 줄의 i번째 줄에는 i번 직원이 할 수 있는 일의 개수와 할 수 있는 일의 번호가 주어진다.
+ * 
+ * [출력]
+ * 첫째 줄에 강호네 회사에서 할 수 있는 일의 개수를 출력한다.
+ * 
+ */
+
+public class BM_11377 {
+	static int n,m,k;
+	static boolean[] visited;
+	static int[] match;
+	static List<Integer>[] adj;
+    
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());	// 일을 최대 2개 할 수 있는 인원 수
+		
+		adj = new ArrayList[n+1];
+		for(int i=1;i<=n;i++) {
+			adj[i] = new ArrayList<>();
+		}
+		visited = new boolean[n+1];
+		match = new int[m+1];
+		
+		
+		for(int i=1;i<=n;i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int count = Integer.parseInt(st.nextToken());
+			for(int j=0;j<count;j++) {
+				adj[i].add(Integer.parseInt(st.nextToken()));
+			}
+		}
+		
+		System.out.println(bmatch());
+	}
+	
+	public static int bmatch() {
+		int count = 0;
+		for(int i=1;i<=n;i++) {
+			if(dfs(i)) {
+				Arrays.fill(visited, false);
+				count++;
+			}
+		}
+		
+		//2번씩 작업할 수 있는 사람 매칭
+		int extra = 0;
+		for(int i=1;i<=n && extra < k;i++) {
+			if(dfs(i)) {
+				Arrays.fill(visited, false);
+				extra++;
+			}
+		}
+		
+		return count+extra;
+	}
+	
+	public static boolean dfs(int x) {
+		if(visited[x]) return false;
+		
+		visited[x] = true;
+		for(int i=0;i<adj[x].size();i++) {
+			int t = adj[x].get(i);
+			if(match[t] == 0 || dfs(match[t])) {
+				match[t] = x;
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
---
+++ baekjoon/src/baekjoon/bipartiteMatching/BM_1671.java
@@ -0,0 +1,96 @@
+package baekjoon.bipartiteMatching;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * 상어의 저녁식사 1671
+ * 어떤 상어는 저녁식사로 서로를 먹는다. 모든 상어는 자신과 다른 상어의 크기, 속도, 지능을 수치로 나타낸 것을 알고 있다. 
+ * 만약, 상어 A의 크기, 속도, 지능이 상어 B의 크기, 속도, 지능보다 크거나 같다면 상어 A는 상어 B를 먹을 수 있다. 
+ * 그러나, 상어들의 왕 김재홍은 상어들이 많이 없어지는 것을 방지하기 위해서 한 상어가 최대 두 개의 상어만 먹을 수 있게 했다. 상어들은 김재홍의 말을 모두 듣는다.
+ * 능력치가 모두 같은 상어 A, B가 있다면 A가 B를, B가 A를 잡아먹을 수는 있지만 A, B가 서로 잡아먹을수는 없다.
+ * N마리 상어의 크기, 속도, 지능이 주어졌을 때, 살아남을 수 있는 상어 수의 최솟값을 구하시오.
+ * 
+ * [입력]
+ * 첫째 줄에 상어의 마리 수 N이 주어진다. 이 값은 50보다 작거나 같은 자연수이다. 둘째 줄부터 각 상어의 크기, 속도, 지능의 정보가 주어진다. 
+ * 이 값은 2,000,000,000보다 작거나 같은 자연수이다.
+ * 
+ * [출력]
+ * 첫째 줄에 살아남을 수 있는 상어 수의 최솟값을 출력한다.
+ */
+public class BM_1671 {
+	static int n;
+	static int shark[][];
+	static List<Integer> adj[];
+	static boolean visited[];
+	static int match[];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		//상어 수
+		n = Integer.parseInt(br.readLine());
+		
+		adj = new ArrayList[n+1];
+		for(int i=1;i<=n;i++) {
+			adj[i] = new ArrayList<>();
+		}
+		visited = new boolean[n+1];
+		match = new int[n+1];
+		shark = new int[n+1][3];
+		
+		// 각 상어의 크기, 속도, 지능을 이차원 배열(shark)에 저장
+		for(int i=1;i<=n;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			shark[i][0] = Integer.parseInt(st.nextToken());	 //상어의 크기
+			shark[i][1] = Integer.parseInt(st.nextToken()); // 상어의 속도
+			shark[i][2] = Integer.parseInt(st.nextToken()); // 상어의 지능
+		}
+		
+		// 상어들의 크기, 속도, 지능을 비교하여 잡아먹을 수 있는 상어들을 리스트에 추가
+		for(int i=1;i<=n;i++) {
+			for(int j=i+1;j<=n;j++) {
+				if(shark[i][0]>=shark[j][0] && shark[i][1]>=shark[j][1] && shark[i][2]>=shark[j][2]) {
+					adj[i].add(j);
+				}else if(shark[i][0]<=shark[j][0] && shark[i][1]<=shark[j][1] && shark[i][2]<=shark[j][2]) {
+					adj[j].add(i);
+				}
+			}
+		}
+		
+		System.out.println(bmatch());
+	}
+	
+	public static int bmatch() {
+		int count = 0;
+		
+		//최대 2마리 까지 잡아 먹을 수 있으므로 2번 매칭
+		for(int i=1;i<=n;i++) {
+			for(int j=0;j<2;j++) {
+				if(dfs(i)) {
+					Arrays.fill(visited, false);
+					count++;
+				}
+			}
+		}
+		
+		return n - count;
+	}
+	
+	public static boolean dfs(int x) {
+		if(visited[x]) return false;
+		
+		visited[x] = true;
+		for(int i=0;i<adj[x].size();i++) {
+			int t = adj[x].get(i);
+			if(match[t] == 0 || dfs(match[t])) {
+				match[t] = x;
+				return true;
+			}
+		}
+		return false;
+	}
+}
