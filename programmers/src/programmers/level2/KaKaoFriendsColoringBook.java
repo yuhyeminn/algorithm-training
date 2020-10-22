6ee1dd43ed67c648e77d6055dad1e02f01bcc3d3
unknown <yuhyemin96@gmail.com>
2020-10-22 16:51:46 +0900

    201022 프로그래머스 lv2 카카오 프렌즈 컬러링 북 solve

--- programmers/src/programmers/level2/KaKaoFriendsColoringBook.java
+++ programmers/src/programmers/level2/KaKaoFriendsColoringBook.java
@@ -1,19 +1,89 @@
 package programmers.level2;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 카카오 프렌즈 컬러링북 
  *
  * [문제]
- * 출판사의 편집자인 어피치는 네오에게 컬러링북에 들어갈 원화를 그려달라고 부탁하여 여러 장의 그림을 받았다. 
- * 여러 장의 그림을 난이도 순으로 컬러링북에 넣고 싶었던 어피치는 영역이 많으면 색칠하기가 까다로워 어려워진다는 사실을 발견하고 그림의 난이도를 영역의 수로 정의하였다.
- *  (영역이란 상하좌우로 연결된 같은 색상의 공간을 의미한다.)
-
-그림에 몇 개의 영역이 있는지와 가장 큰 영역의 넓이는 얼마인지 계산하는 프로그램을 작성해보자.
+ * https://programmers.co.kr/learn/courses/30/lessons/1829
+ * 
+ * [참고]
+ * https://swycha.tistory.com/141
  */
 public class KaKaoFriendsColoringBook {
 
-	public static void main(String[] args) {
+	static int[] dx = {1, -1, 0, 0};	//x좌표 방향배열
+	static int[] dy = {0, 0, 1, -1};	//y좌표 방향배열
+	static Queue<Point> queue = new LinkedList<>();
+	static boolean[][] visited;
+	static int size = 0;		//칸의 개수
+	
+	public static int[] solution(int m, int n, int[][] picture) {
+		int area_cnt = 0;
+		int max_size_area = 0;
 		
+		visited = new boolean[m][n];
+		
+		for(int i=0; i<m; i++) {
+			for(int j=0;j<n;j++) {
+				if(picture[i][j] > 0 && visited[i][j] != true) {
+					size = 1;
+					bfs(picture, i, j, m, n);
+					area_cnt++;
+					if(max_size_area < size) {
+						max_size_area = size;
+					}
+				}
+			}
+		}
+		
+		int answer[] = new int[2];
+		answer[0] = area_cnt;
+		answer[1] = max_size_area;
+		
+		return answer;
 	}
 	
+	public static void bfs(int[][] picture, int x, int y, int m, int n) {
+		queue.add(new Point(x,y));
+		visited[x][y] = true;
+		
+		while(!queue.isEmpty()) {
+			Point p = queue.poll();
+			
+			//상하좌우 모두 탐색
+			for(int i=0;i<4;i++) {
+				int px = p.x + dx[i];
+				int py = p.y + dy[i];
+				
+				if(0 <= px && px < m && 0 <= py && py < n) {
+					if(picture[px][py] == picture[x][y] && visited[px][py] != true) {
+						queue.add(new Point(px,py));
+						visited[px][py] = true;
+						size++;	//지나온 칸의 개수(영역 사이즈)
+					}
+				}
+			}
+		}
+	}
+	
+	static class Point{
+		int x;
+		int y;
+		
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int m = 6;
+		int n = 4;
+		int picture[][] = {{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}};
+		System.out.println("result = " + Arrays.toString(solution(m, n, picture)));
+	}
 }
