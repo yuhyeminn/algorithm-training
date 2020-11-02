9ba8af104b1832df1fe3004e56c5abb563c9ef6d
unknown <yuhyemin96@gmail.com>
2020-11-02 16:40:42 +0900

    201102 윈터코딩 1번 solve

--- programmers/src/programmers/wintercoding/Winter01.java
+++ programmers/src/programmers/wintercoding/Winter01.java
@@ -1,46 +1,65 @@
 package programmers.wintercoding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Winter01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int n = 6;
 		int delivery[][] = { { 1, 3, 1 }, { 3, 5, 0 }, { 5, 4, 0 }, { 2, 5, 0 } };
 		System.out.println(solution(n, delivery));
 	}
 
 	public static String solution(int n, int[][] delivery) {
-		char products[] = new char[n + 1]; // 상품별 재고 여부
+		char[] stock = new char[n + 1]; // 상품별 재고 여부
+		Product[] products = new Product[n+1];
+		
 		for (int i = 1; i < n + 1; i++) {
-			products[i] = '?';
+			stock[i] = '?';
+			products[i] = new Product(i);
 		}
-
+		
 		for (int i = 0; i < delivery.length; i++) {
 			int p1 = delivery[i][0];
 			int p2 = delivery[i][1];
-			boolean isDelivery = delivery[i][2] == 0 ? false : true;
 
+			products[p1].pack.add(p2);
+			products[p2].pack.add(p1);
+			
 			// 배송 됐을 경우
-			if (isDelivery) {
-				products[p1] = 'o';
-				products[p2] = 'o';
+			if (delivery[i][2] == 1) {
+				stock[p1] = stock[p2] = 'o';
 			}
-			// 배송되지 않았을 경우
-			else {
-				if (products[p1] == 'o') {
-					products[p2] = 'x';
-				} else if (products[p2] == 'o') {
-					products[p1] = 'x';
+		}
+		
+		//**배송된 경우를 먼저 처리해주고 나머지 상품에 대한 재고상태를 처리함
+		for(int i=1;i<stock.length;i++) {
+			//재고있는 상품과 같이 포장되었으나 재고상태가 ?인 경우는 배송된 것이 아니므로 재고없음으로 처리
+			if(stock[i] == 'o') {
+				for(int p : products[i].pack) {
+					if(stock[p] != 'o') {
+						stock[p] = 'x';
+					}
 				}
 			}
 		}
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 1; i < n + 1; i++) {
-			sb.append(products[i]);
+			sb.append(stock[i]);
 		}
 		
 		return sb.toString();
 	}
+	
+	static class Product {
+	    int num;
+	    List<Integer> pack;
 
+	    public Product(int num) {
+	        this.num = num;
+	        this.pack = new ArrayList<>();
+	    }
+	}
 }
